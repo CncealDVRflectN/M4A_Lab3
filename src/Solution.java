d970ce395f51dddd32c56905742ba1d1a9c412d6
TTOJlTOPA <zhuk.valeriy.98@gmail.com>
2017-05-12 00:27:21 +0300

    fix2

--- src/Solution.java
+++ src/Solution.java
@@ -2,9 +2,9 @@ public class Solution {
     private static final double epsilon = 0.000001;
 
     public static void main(String[] args) {
-        double x0 = 4.1;
+        double x0 = 3.75;
         double xcur;
-        double xnext = 3.6897675823978724;
+        double xnext = 3.556548540294219;
         int iterations = 0;
         System.out.println("Начальное приближение: " + x0);
         do {
@@ -15,9 +15,18 @@ public class Solution {
         System.out.println("Решение: " + xnext);
         System.out.println("Невязка: " + Math.abs(calculateF(xnext)));
         System.out.println("Количество итераций: " + iterations);
+        System.out.println("const: " + (-calculateFSecondDeriv(xnext) / (2 * calculateFDeriv(xnext))));
     }
 
     private static double calculateF(double x) {
         return Math.pow(Math.E, x) + Math.log(x) - 10 * x;
+    }
+
+    private static double calculateFDeriv(double x) {
+        return Math.pow(Math.E, x) + 1 / x - 10;
+    }
+
+    private static double calculateFSecondDeriv(double x) {
+        return Math.pow(Math.E, x) - 1 / Math.pow(x, 2);
     }
 }
